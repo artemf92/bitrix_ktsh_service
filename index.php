47a85e8469ade26d7014582ebc0afc6acfd1ab5a
Artem Frolov <artemfrolov@MacBook-Air-Artem.local>
2023-12-08 20:10:32 +0200

    test deploy3

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $indexOptions = $themeSettings['INDEX']['OPTIONS']['SORT_ORDER'];
 $arOrder = explode(',', $indexOptions['VALUE']);
 
 ?>
-test deploy3
+test deploy4
 <div class="b-home-content">
 <?php //home slider?>
     <div class="b-home-block" data-order="-1">
