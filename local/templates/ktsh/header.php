24ceb9c0d4fcac6c256bfa993336f014372399a6
Artem Frolov <artemfrolov@MacBook-Air-Artem.local>
2023-12-11 08:06:10 +0200

    Добавил фавиконку

--- local/templates/ktsh/header.php
+++ local/templates/ktsh/header.php
@@ -12,15 +12,16 @@ if (!Loader::includeModule('gedestudio.axiland')) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="format-detection" content="telephone=no">
-    <link rel="icon" type="image/x-icon" href="<?=SITE_TEMPLATE_PATH?>/favicon.png"/>
-    <link rel="icon" type="image/png" sizes="64x64" href="<?=SITE_TEMPLATE_PATH?>/favicon64.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?=SITE_TEMPLATE_PATH?>/favicon32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?=SITE_TEMPLATE_PATH?>/favicon16.png">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css"  crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon.png" />
+    <link rel="icon" type="image/png" sizes="64x64" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH ?>/images/favicon16.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600;700&family=Exo+2:ital,wght@0,600;0,700;1,600;1,700&family=Merriweather:ital,wght@0,700;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,600;1,700&family=Open+Sans:ital,wght@0,700;1,600;1,700&family=Roboto:ital,wght@0,700;1,500;1,700&family=Ubuntu:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
     <? $APPLICATION->ShowMeta("keywords"); ?>
@@ -37,76 +38,77 @@ if (!Loader::includeModule('gedestudio.axiland')) {
     <? $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/jquery.min.js"); ?>
     <? $APPLICATION->IncludeFile(
         SITE_DIR . "include/codes/head.php",
-        Array(),
-        Array("MODE" => "php")
+        array(),
+        array("MODE" => "php")
     ); ?>
 </head>
 <?
 $curPage = $APPLICATION->GetCurPage(true);
 $bodyClass = "";
-if ($curPage == SITE_DIR . "index.php"):
+if ($curPage == SITE_DIR . "index.php") :
     $bodyClass = "b-home-page";
-else:
+else :
     $bodyClass = "b-other-page";
 endif;
-if($APPLICATION->GetProperty("without_h1") == "Y"){
+if ($APPLICATION->GetProperty("without_h1") == "Y") {
     $bodyClass = $bodyClass . " b-without-h1";
 }
 
 ?>
-<body class="<?= $bodyClass; ?> <?=Gedestudio::pageClass()?>"  data-site-dir="<?=SITE_DIR?>">
-<? $APPLICATION->IncludeFile(
-    SITE_DIR . "include/codes/body.php",
-    Array(),
-    Array("MODE" => "php")
-); ?>
-<?php
 
-$APPLICATION->ShowPanel();
-$themeSettings = $APPLICATION->IncludeComponent('gedestudio.axiland:theme.settings', '', array(), false, array('HIDE_ICONS' => 'Y'));
-$blackHeader = $themeSettings['COLORS']['OPTIONS']['BLACK_HEADER']['VALUE'] ?? false;
-$blackHeaderMob = $themeSettings['COLORS']['OPTIONS']['BLACK_HEADER_MOB']['VALUE'] ?? false;
+<body class="<?= $bodyClass; ?> <?= Gedestudio::pageClass() ?>" data-site-dir="<?= SITE_DIR ?>">
+    <? $APPLICATION->IncludeFile(
+        SITE_DIR . "include/codes/body.php",
+        array(),
+        array("MODE" => "php")
+    ); ?>
+    <?php
 
-if($blackHeader == 'Y'){
-    $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/black-header.css");
-}
-if($blackHeaderMob == 'Y'){
-    $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/black-header-mob.css");
-}
+    $APPLICATION->ShowPanel();
+    $themeSettings = $APPLICATION->IncludeComponent('gedestudio.axiland:theme.settings', '', array(), false, array('HIDE_ICONS' => 'Y'));
+    $blackHeader = $themeSettings['COLORS']['OPTIONS']['BLACK_HEADER']['VALUE'] ?? false;
+    $blackHeaderMob = $themeSettings['COLORS']['OPTIONS']['BLACK_HEADER_MOB']['VALUE'] ?? false;
 
-$showMobileSlogan = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_SLOGAN']['VALUE'] ?? false;
-$showMobileAddress = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_ADDRESS']['VALUE'] ?? false;
-$showMobilePhone = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_PHONE']['VALUE'] ?? false;
-$showMobileSocials = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_SOCIALS']['VALUE'] ?? false;
+    if ($blackHeader == 'Y') {
+        $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/black-header.css");
+    }
+    if ($blackHeaderMob == 'Y') {
+        $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/black-header-mob.css");
+    }
 
-$mobileSloganClass = '';
-if($showMobileSlogan != 'Y'){
-    $mobileSloganClass = 'b-hide-mobile';
-}
-$mobileAddressClass = '';
-if($showMobileAddress != 'Y'){
-    $mobileAddressClass = 'b-hide-mobile';
-}
-$mobilePhoneClass = '';
-if($showMobilePhone != 'Y'){
-    $mobilePhoneClass = 'b-hide-mobile';
-}
-$mobileSocialsClass = '';
-if($showMobileSocials != 'Y'){
-    $mobileSocialsClass = 'b-hide-mobile';
-}
+    $showMobileSlogan = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_SLOGAN']['VALUE'] ?? false;
+    $showMobileAddress = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_ADDRESS']['VALUE'] ?? false;
+    $showMobilePhone = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_PHONE']['VALUE'] ?? false;
+    $showMobileSocials = $themeSettings['INDEX']['OPTIONS']['BLOCK_MOBILE_HEADER_SETTING_SOCIALS']['VALUE'] ?? false;
 
-?>
-<? CJSCore::Init(); ?>
-<div class="site-wrap">
+    $mobileSloganClass = '';
+    if ($showMobileSlogan != 'Y') {
+        $mobileSloganClass = 'b-hide-mobile';
+    }
+    $mobileAddressClass = '';
+    if ($showMobileAddress != 'Y') {
+        $mobileAddressClass = 'b-hide-mobile';
+    }
+    $mobilePhoneClass = '';
+    if ($showMobilePhone != 'Y') {
+        $mobilePhoneClass = 'b-hide-mobile';
+    }
+    $mobileSocialsClass = '';
+    if ($showMobileSocials != 'Y') {
+        $mobileSocialsClass = 'b-hide-mobile';
+    }
+
+    ?>
+    <? CJSCore::Init(); ?>
+    <div class="site-wrap">
         <header class="header">
             <div class="container">
                 <div class="b-up b-flex">
                     <div class="d-lg-none b-mobile-burger">
                         <button class="hamburger hamburger--slider" type="button">
-						<span class="hamburger-box">
-							<span class="hamburger-inner"><?=GetMessage('GEDE_HEADER_MENU')?></span>
-						</span>
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"><?= GetMessage('GEDE_HEADER_MENU') ?></span>
+                            </span>
                             <span class="b-burger-text"></span>
                         </button>
                     </div>
@@ -115,45 +117,45 @@ if($showMobileSocials != 'Y'){
                             <a href="/">
                                 <? $APPLICATION->IncludeFile(
                                     SITE_DIR . "include/header/logo.php",
-                                    Array(),
-                                    Array("MODE" => "php")
+                                    array(),
+                                    array("MODE" => "php")
                                 ); ?>
                             </a>
                         </div>
-                        <div class="b-slogan <?=$mobileSloganClass?>">
+                        <div class="b-slogan <?= $mobileSloganClass ?>">
                             <? $APPLICATION->IncludeFile(
                                 SITE_DIR . "include/header/slogan.php",
-                                Array(),
-                                Array("MODE" => "php")
+                                array(),
+                                array("MODE" => "php")
                             ); ?>
                         </div>
                     </div>
-                    <div class="b-center b-header-address <?=$mobileAddressClass?>">
+                    <div class="b-center b-header-address <?= $mobileAddressClass ?>">
                         <i class="icon-placeholder-filled-point"></i>
                         <span>
                             <? $APPLICATION->IncludeFile(
                                 SITE_DIR . "include/header/address.php",
-                                Array(),
-                                Array("MODE" => "php")
+                                array(),
+                                array("MODE" => "php")
                             ); ?>
                         </span>
                     </div>
                     <div class="b-right b-flex">
-                        <div class="b-header-phone <?=$mobilePhoneClass?>">
+                        <div class="b-header-phone <?= $mobilePhoneClass ?>">
                             <i class="icon-phone-call"></i>
                             <span>
                                 <? $APPLICATION->IncludeFile(
                                     SITE_DIR . "include/header/phone.php",
-                                    Array(),
-                                    Array("MODE" => "php")
+                                    array(),
+                                    array("MODE" => "php")
                                 ); ?>
                             </span>
                         </div>
-                        <div class="b-header-socials <?=$mobileSocialsClass?>">
+                        <div class="b-header-socials <?= $mobileSocialsClass ?>">
                             <? $APPLICATION->IncludeFile(
                                 SITE_DIR . "include/header/socials.php",
-                                Array(),
-                                Array("MODE" => "php")
+                                array(),
+                                array("MODE" => "php")
                             ); ?>
                         </div>
 
@@ -162,17 +164,17 @@ if($showMobileSocials != 'Y'){
                 <div class="b-down">
                     <? $APPLICATION->IncludeFile(
                         SITE_DIR . "include/header/header-menu.php",
-                        Array(),
-                        Array("MODE" => "php")
+                        array(),
+                        array("MODE" => "php")
                     ); ?>
                 </div>
             </div>
         </header>
 
         <div class="b-content">
-            <? if ($curPage != SITE_DIR . "index.php"): ?>
+            <? if ($curPage != SITE_DIR . "index.php") : ?>
                 <div class="b-site-inner">
-                    <? if($APPLICATION->GetProperty("hide_bc") != "Y"): ?>
+                    <? if ($APPLICATION->GetProperty("hide_bc") != "Y") : ?>
                         <div class="container">
                             <? $APPLICATION->IncludeComponent(
                                 "bitrix:breadcrumb",
@@ -188,11 +190,11 @@ if($showMobileSocials != 'Y'){
                         </div>
                     <? endif; ?>
 
-                    <? if($APPLICATION->GetProperty("hide_h1") != "Y"): ?>
+                    <? if ($APPLICATION->GetProperty("hide_h1") != "Y") : ?>
                         <div class="container">
                             <div class="b-page-title">
                                 <h1><? $APPLICATION->ShowTitle() ?></h1>
                             </div>
                         </div>
-                    <? endif;?>
-            <? endif ?>
+                    <? endif; ?>
+                <? endif ?>
