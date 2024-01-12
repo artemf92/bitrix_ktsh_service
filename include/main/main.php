5e809441247217b3eb39f9e83f7d933500c00695
Artem Frolov <artemfrolov@MacBook-Air-Artem.local>
2024-01-12 12:09:45 +0200

    переделал вывод форм на сайте, чтобы можно было динамически заполнять поля в формах б24

--- include/main/main.php
+++ include/main/main.php
@@ -5,11 +5,11 @@
 			<a href="#block-services" class="btn btn_white popup_message__open">Выбрать услугу</a>
 		</div>
 		<div class="intro__cell">
-			<div class="intro__text"><img draggable="false" src="https://pik-comfort.ru/static/img/icons/questions.svg"> Есть
+			<div class="intro__text"><img draggable="false" src="<?=SITE_TEMPLATE_PATH?>/images/questions.svg"> Есть
 				вопросы<br>или предложения?</div><button data-toggle="modal" data-target="#b-zapis-form" data-form-title="Оставить заявку" class="btn btn_white popup_message__open">Написать нам</button>
 		</div>
 		<div class="intro__cell">
-			<div class="intro__text"><img draggable="false" src="https://pik-comfort.ru/static/img/icons/cabinet.svg">
+			<div class="intro__text"><img draggable="false" src="<?=SITE_TEMPLATE_PATH?>/images/cabinet.svg">
 				Оплатить квитанции и&nbsp;внести показания счетчиков</div><a href="https://dom.gosuslugi.ru/" target="_blank" rel="nofollow" class="btn btn_white">Войти в кабинет</a>
 		</div>
 	</div>
--- index.php
+++ index.php
@@ -166,7 +166,6 @@ $arOrder = explode(',', $indexOptions['VALUE']);
         }
         ?>
         <div id="block-services" class="b-home-block" data-order="<?= array_keys($arOrder, "BLOCK_CATALOG")[0]; ?>" style="background-image: url(<?= $bgImage ?>); background-position: 50% 50%; background-size: cover; background-color: <?= $themeSettings['INDEX']['OPTIONS']['BLOCK_CATALOG_BACKGROUND_COLOR']['VALUE'] ?>">
-
             <div class="b-catalog-list-block b-block">
                 <div class="container">
                     <div class="b-block-title-wrap">
@@ -186,40 +185,40 @@ $arOrder = explode(',', $indexOptions['VALUE']);
                         </div>
                     </div>
                     <? $APPLICATION->IncludeComponent(
-	"bitrix:catalog.section.list", 
-	"catalog", 
-	array(
-		"VIEW_MODE" => "TEXT",
-		"SHOW_PARENT_NAME" => "Y",
-		"IBLOCK_TYPE" => "content",
-		"IBLOCK_ID" => "2",
-		"SECTION_CODE" => "",
-		"SECTION_URL" => "",
-		"COUNT_ELEMENTS" => "N",
-		"TOP_DEPTH" => "2",
-		"SECTION_FIELDS" => array(
-			0 => "",
-			1 => "",
-		),
-		"SECTION_USER_FIELDS" => array(
-			0 => "",
-			1 => "",
-		),
-		"ADD_SECTIONS_CHAIN" => "N",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_NOTES" => "",
-		"CACHE_GROUPS" => "Y",
-		"COMPONENT_TEMPLATE" => "catalog",
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],
-		"COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
-		"ADDITIONAL_COUNT_ELEMENTS_FILTER" => "additionalCountFilter",
-		"HIDE_SECTIONS_WITH_ZERO_COUNT_ELEMENTS" => "N",
-		"FILTER_NAME" => "sectionsFilter",
-		"CACHE_FILTER" => "N"
-	),
-	false
-); ?>
+                        "bitrix:catalog.section.list",
+                        "catalog",
+                        array(
+                            "VIEW_MODE" => "TEXT",
+                            "SHOW_PARENT_NAME" => "Y",
+                            "IBLOCK_TYPE" => "content",
+                            "IBLOCK_ID" => "2",
+                            "SECTION_CODE" => "",
+                            "SECTION_URL" => "",
+                            "COUNT_ELEMENTS" => "N",
+                            "TOP_DEPTH" => "2",
+                            "SECTION_FIELDS" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "SECTION_USER_FIELDS" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "CACHE_TYPE" => "A",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_NOTES" => "",
+                            "CACHE_GROUPS" => "Y",
+                            "COMPONENT_TEMPLATE" => "catalog",
+                            "SECTION_ID" => $_REQUEST["SECTION_ID"],
+                            "COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
+                            "ADDITIONAL_COUNT_ELEMENTS_FILTER" => "additionalCountFilter",
+                            "HIDE_SECTIONS_WITH_ZERO_COUNT_ELEMENTS" => "N",
+                            "FILTER_NAME" => "sectionsFilter",
+                            "CACHE_FILTER" => "N"
+                        ),
+                        false
+                    ); ?>
                 </div>
             </div>
         </div>
@@ -319,72 +318,72 @@ $arOrder = explode(',', $indexOptions['VALUE']);
                         </div>
                     </div>
                     <? $APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"akcii-list-ajax", 
-	array(
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"AJAX_MODE" => "Y",
-		"IBLOCK_TYPE" => "content",
-		"IBLOCK_ID" => "1",
-		"NEWS_COUNT" => "3",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_ORDER1" => "DESC",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER2" => "ASC",
-		"FILTER_NAME" => "",
-		"FIELD_CODE" => array(
-			0 => "ID",
-			1 => "",
-		),
-		"PROPERTY_CODE" => array(
-			0 => "PRICE",
-			1 => "",
-		),
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"SET_TITLE" => "N",
-		"SET_BROWSER_TITLE" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "3600",
-		"CACHE_FILTER" => "Y",
-		"CACHE_GROUPS" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"PAGER_TITLE" => "Новости",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => "",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "Y",
-		"PAGER_BASE_LINK_ENABLE" => "Y",
-		"SET_STATUS_404" => "N",
-		"SHOW_404" => "N",
-		"MESSAGE_404" => "",
-		"PAGER_BASE_LINK" => "/news/",
-		"PAGER_PARAMS_NAME" => "arrPager",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"COMPONENT_TEMPLATE" => "akcii-list-ajax",
-		"STRICT_SECTION_CHECK" => "N"
-	),
-	false
-); ?>
+                        "bitrix:news.list",
+                        "akcii-list-ajax",
+                        array(
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "AJAX_MODE" => "Y",
+                            "IBLOCK_TYPE" => "content",
+                            "IBLOCK_ID" => "1",
+                            "NEWS_COUNT" => "3",
+                            "SORT_BY1" => "ACTIVE_FROM",
+                            "SORT_ORDER1" => "DESC",
+                            "SORT_BY2" => "SORT",
+                            "SORT_ORDER2" => "ASC",
+                            "FILTER_NAME" => "",
+                            "FIELD_CODE" => array(
+                                0 => "ID",
+                                1 => "",
+                            ),
+                            "PROPERTY_CODE" => array(
+                                0 => "PRICE",
+                                1 => "",
+                            ),
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "PREVIEW_TRUNCATE_LEN" => "",
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "SET_TITLE" => "N",
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                            "PARENT_SECTION" => "",
+                            "PARENT_SECTION_CODE" => "",
+                            "INCLUDE_SUBSECTIONS" => "Y",
+                            "CACHE_TYPE" => "A",
+                            "CACHE_TIME" => "3600",
+                            "CACHE_FILTER" => "Y",
+                            "CACHE_GROUPS" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "DISPLAY_BOTTOM_PAGER" => "Y",
+                            "PAGER_TITLE" => "Новости",
+                            "PAGER_SHOW_ALWAYS" => "N",
+                            "PAGER_TEMPLATE" => "",
+                            "PAGER_DESC_NUMBERING" => "N",
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                            "PAGER_SHOW_ALL" => "Y",
+                            "PAGER_BASE_LINK_ENABLE" => "Y",
+                            "SET_STATUS_404" => "N",
+                            "SHOW_404" => "N",
+                            "MESSAGE_404" => "",
+                            "PAGER_BASE_LINK" => "/news/",
+                            "PAGER_PARAMS_NAME" => "arrPager",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "COMPONENT_TEMPLATE" => "akcii-list-ajax",
+                            "STRICT_SECTION_CHECK" => "N"
+                        ),
+                        false
+                    ); ?>
                 </div>
             </div>
         </div>
@@ -1508,7 +1507,7 @@ $arOrder = explode(',', $indexOptions['VALUE']);
             </div>
         </div>
     </div>
-    
+
     <div id="block-safety" class="b-home-block" data-order="8">
         <div class="b-documents-block b-block">
             <div class="container">
