bd280f77f96b71ec9f4147f25eb17a8a5f64ec3a
Artem Frolov <artemfrolov@MacBook-Air-Artem.local>
2024-01-25 17:55:47 +0200

    Правки от 25.01.24

---
+++ local/php_interface/migrations/Version20240125185517.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20240125185517 extends Version
+{
+    protected $description   = "Правки блока \"Режим работы\" v2";
+    protected $moduleVersion = "4.6.1";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->saveElementByXmlId(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(10)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                     ->Iblock()
+                     ->deleteElementByXmlId(
+                         $item['iblock_id'],
+                         $item['fields']['XML_ID']
+                     );
+             });
+    }
+}
